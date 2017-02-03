66faf85cddb88d4b24c0162735d32837eddd7394
tinghom <tinghom@gmail.com>
2017-02-03 16:16:58 +0800

    fix: demo 使用CheckOut()直接跳轉緑界.

--- src/Controllers/DemoController.php
+++ src/Controllers/DemoController.php
@@ -56,6 +56,7 @@ class DemoController extends Controller
 
         //Go to EcPay
         echo "緑界頁面導向中...";
-        echo Ecpay::i()->CheckOutString();
+        // echo Ecpay::i()->CheckOutString();
+        Ecpay::i()->CheckOut();
     }
 }
