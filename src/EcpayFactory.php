b90ecf25768aa9ab6510352acb7bf7f139444dd2
tinghom <tinghom@gmail.com>
2017-02-06 10:37:53 +0800

    fix: CheckOutFeedback() call ECPay_CheckOutFeedback:: CheckOut() namespace.

--- src/EcpayFactory.php
+++ src/EcpayFactory.php
@@ -7,6 +7,6 @@ class EcpayFactory extends \ECPay_AllInOne
     //取得付款結果通知的方法
     function CheckOutFeedback($ecpayPost = null) {
         $ecpayPost = $ecpayPost? $ecpayPost : $_POST;
-        return $arFeedback = ECPay_CheckOutFeedback::CheckOut($ecpayPost,$this->HashKey,$this->HashIV,0);   
+        return $arFeedback = \ECPay_CheckOutFeedback::CheckOut($ecpayPost,$this->HashKey,$this->HashIV,0);   
     }
 }
