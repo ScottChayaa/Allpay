6417728c5a43db68b6dc9b4e0683eec82b18191d
tinghom <tinghom@gmail.com>
2017-05-24 17:57:19 +0800

    fix: EcpayServiceProvider share ecpay 方式變更.(Laravel 5.4已無share)

--- src/EcpayServiceProvider.php
+++ src/EcpayServiceProvider.php
@@ -8,40 +8,35 @@ class EcpayServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap the application services.
-     *
-     * @return void
      */
     public function boot()
     {
         //Route
-        include __DIR__ . '/routes.php';
+        include __DIR__.'/routes.php';
 
         //Language
-        $this->loadTranslationsFrom(__DIR__ . '/Lang', 'ecpay');
+        $this->loadTranslationsFrom(__DIR__.'/Lang', 'ecpay');
 
         //Publish Config
         $this->publishes([
-            __DIR__ . '/Config/ecpay.php' => config_path('ecpay.php'),
+            __DIR__.'/Config/ecpay.php' => config_path('ecpay.php'),
         ], 'config');
     }
 
     /**
      * Register the application services.
-     *
-     * @return void
      */
     public function register()
     {
         //Config
-        $this->mergeConfigFrom(__DIR__ . '/Config/ecpay.php', 'ecpay');
+        $this->mergeConfigFrom(__DIR__.'/Config/ecpay.php', 'ecpay');
 
         //View
-        $this->loadViewsFrom(__DIR__ . '/Views', 'ecpay');
+        $this->loadViewsFrom(__DIR__.'/Views', 'ecpay');
 
         //Facade => Custom Class
-        $this->app['ecpay'] = $this->app->share(function ($app) {
-            return new Ecpay;
+        $this->app->singleton('ecpay', function ($app) {
+            return new Ecpay();
         });
-
     }
 }
